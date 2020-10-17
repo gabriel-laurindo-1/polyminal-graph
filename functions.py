c285cfb099dc37ebc4ab66b6c2c006b4460696b3
Gabriel Laurindo <gabriel.laurindo.1@outlook.com>
2020-10-17 14:07:57 -0300

    Criação das funções de validação form

--- functions.py
+++ functions.py
@@ -1,8 +1,33 @@
-from numpy import linspace
+from numpy import linspace, around
 
-def str_to_list(list_value: list):
-    list_coeffs = list_value.replace(' ', '').split(',')
-    return list(map(float, list_coeffs))
+def exist_letters(string: str):
+    """
+    ### exist_letters
+
+    Verifica e existem caracteres do tipo texto.
+
+    `string`: texto de entrada
+
+    """
+    char_permited = [',', '-', '.', ' ']
+    for char in char_permited:
+        string = string.replace(char, '')
+    return not all(char.isdigit() for char in string)
+
+def str_to_list(text: str):
+    """
+    ### str_to_list
+
+    Transforma uma string em uma lista numérica, 
+    onde cada elemento é do tipo `float`.
+
+    `text`: String que será transformada em uma lista. 
+    """
+    try:
+	    list_coeffs = text.replace(' ', '').split(',')
+	    return list(map(float, list_coeffs))
+    except:
+	    return []
 
 def Polynomial(P: list, x: list):
     """
@@ -16,10 +41,10 @@ def Polynomial(P: list, x: list):
     """
     return sum(c * x ** e for (e, c) in enumerate(P))
 
-def generator_points(down_limit, upper_limit, n_points):
+def generator_points(down_limit, upper_limit, n_points, precision=4):
     """
     # Generator points
     
     `return`: numpy.array no intervalo informado.
     """
-    return linspace(down_limit, upper_limit, n_points, endpoint=True)
+    return around(linspace(down_limit, upper_limit, n_points, endpoint=True), decimals=precision)
